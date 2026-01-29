d912a2c2f3e0cbd020147fbc93f6360d6e66043b
WillJ1259 <xj2181@nyu.edu>
2026-01-29 15:05:43 -0500

    Update part1_lists_as_table.py

--- part1_lists_as_table.py
+++ part1_lists_as_table.py
@@ -12,14 +12,22 @@ students = [
 # columns: [id, name, school, credits]
 
 # TODO 1: Print all student names (one per line)
-
+for s in students:
+    print(s[1])
 # TODO 2: Print only the students in GSAS (id and name)
-
+for s in students:
+    if s[2] == "GSAS":
+        print(s[1])
 # TODO 3: Print students with credits > 30 (name and credits)
-
+for s in students:
+    if s[3] > 30:
+        print(s[1],s[3])
 # TODO 4: Insert a new student row for: id=5, name='Eve', school='CAS', credits=22
-
+students.insert(4, [5, "Eve", "CAS", 22])
+print(students)
 # TODO 5: Update Bob’s credits to 30
-
+students[1][3] = 30
+print(students)
 # Reflection (answer in a comment):
 # TODO: What breaks if we insert a new column at position 2?
+# Answer: It will add a new student between Bob and Carol.
